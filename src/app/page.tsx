78b1a945ecc040c4901b83bc1b32acfe6792b1ef
ism178 <ism178@ensign.edu>
2025-05-19 19:35:00 -0600

    finxing contact component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,13 @@
 import { AnimatePresence } from "framer-motion";
 import Image from "next/image";
 import AnimationProvider from "../contexts/AnimationContext";
-import { Contact } from "lucide-react";
 import About from "../components/About";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 import Services from "../components/Services";
 import Testimonials from "../components/Testimonials";
+import Contact from "../components/Contact";
 
 export default function Home() {
   return (
